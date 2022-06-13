61e328e77dd2acd69e20a63f39f29076bceedd89
LagBug <achihait@gmail.com>
2022-06-13 14:38:24 +0300

    Added support for 1.19

--- MapBuilder.java
+++ MapBuilder.java
@@ -30,7 +30,7 @@ public class MapBuilder {
     private boolean renderOnce;
     private boolean isNewestVersion = false;
 
-    private final String[] newVersions = {"1.13", "1.14", "1.15", "1.16", "1.17", "1.18"};
+    private final String[] newVersions = {"1.13", "1.14", "1.15", "1.16", "1.17", "1.18", "1.19"};
 
     public MapBuilder() {
         cursors = new MapCursorCollection();
